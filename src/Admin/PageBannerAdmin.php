c4bcdec738612106de85c2864dc0c824e3154460
Simon Erkelens <simon@pikselin.com>
2023-06-20 14:06:57 +1200

    Add annotations. PSR-2 fixes

--- src/Admin/PageBannerAdmin.php
+++ src/Admin/PageBannerAdmin.php
@@ -1,35 +1,36 @@
 <?php
 
-namespace Pikselin\PageBanners\admin {
+namespace Pikselin\PageBanners\admin;
 
 use Pikselin\PageBanners\DataObjects\PageBanner;
 use SilverStripe\Admin\ModelAdmin;
 
-    class PageBannerAdmin extends ModelAdmin {
+/**
+ * Class \Pikselin\PageBanners\admin\PageBannerAdmin
+ *
+ */
+class PageBannerAdmin extends ModelAdmin
+{
+    private static $icon = 'font-icon-attention';
+    private static $menu_icon_class = 'font-icon-attention';
 
-        private static $icon = 'font-icon-attention';
-        private static $menu_icon_class = 'font-icon-attention';
+    /**
+     *
+     * @var array
+     */
+    private static $managed_models = array(
+        PageBanner::class
+    );
 
-        /**
-         * 
-         * @var array 
-         */
-        private static $managed_models = array(
-            PageBanner::class
-        );
+    /**
+     *
+     * @var string
+     */
+    private static $url_segment = 'page-banners';
 
-        /**
-         *
-         * @var string 
-         */
-        private static $url_segment = 'page-banners';
-
-        /**
-         *
-         * @var string 
-         */
-        private static $menu_title = 'Page banners';
-
-    }
-
+    /**
+     *
+     * @var string
+     */
+    private static $menu_title = 'Page banners';
 }
