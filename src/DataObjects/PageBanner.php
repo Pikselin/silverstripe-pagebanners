d5cdc6327399cec8f850b41c4b4c6864ba730593
Hadlee Lineham <hadlee@pikselin.com>
2023-08-16 13:00:14 +1200

    Updating type of insurance

--- src/DataObjects/PageBanner.php
+++ src/DataObjects/PageBanner.php
@@ -173,7 +173,7 @@ class PageBanner extends DataObject implements PermissionProvider
         $Type = DropdownField::create(
             'Type',
             'Type of alert',
-            $alerts
+            singleton(__CLASS__)->dbObject('Display')->enumValues()
         );
         $fields->addFieldsToTab('Root.Main', [
             $IsGlobal,
